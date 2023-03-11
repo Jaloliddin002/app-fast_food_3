7b66c34ad751e42eea14d9fc9a40b9788af9c2f0
User <oabdumalikov912@gmail.com>
2023-03-11 18:36:26 +0500

    sdfg

---
+++ src/main/java/org/example/model/BaseModel.java
@@ -0,0 +1,10 @@
+package org.example.model;
+
+import lombok.*;
+
+import java.util.UUID;
+@Setter
+@Getter@Builder@AllArgsConstructor@NoArgsConstructor
+public class BaseModel {
+    UUID id;
+}
---
+++ src/main/java/org/example/model/food/Food.java
@@ -0,0 +1,5 @@
+package org.example.model.food;
+
+public class Food {
+
+}
